eb7a9ebe05301eac4d330a828da12c105f87ac81
Jawad Abbasi <jawadabbasi616@gmail.com>
2020-11-13 02:06:08 +0500

    Ending Started

---
+++ WhatsappMessenger/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerViewController.swift
+//  WhatsappMessenger
+//
+//  Created by Jawad Abbasi on 13/11/2020.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
